a88d36d3e28313ca8ef96c75a78653e4e8e40386
陈晓东 <xiaodong3333@126.com>
2019-11-23 18:05:13 +0800

    优化 ui

--- pages/photo/photo.js
+++ pages/photo/photo.js
@@ -32,17 +32,16 @@ Page({
         "url": '../../assets/images/5.jpg',
         "title": "tlt5",
         "id": "index5"
-      },
-      {
-        "url": '../../assets/images/6.jpg',
-        "title": "tlt6",
-        "id": "index6"
       }
     ],
     tip: "下一页",
     tipEnd: "结束",
     toIndex: '',
-    scrollHeight: ''
+    index: 0,
+    scrollTop: '',
+    scrollHeight: '',
+    animation: '',
+    classList: ["bounce", "flash", "pulse", "rubberBand", "shake", "headShake", "swing", "tada", "wobble", "jello", "bounceIn", "bounceInDown", "bounceInLeft", "bounceInRight", "bounceInUp", "bounceOut", "bounceOutDown", "bounceOutLeft", "bounceOutRight", "bounceOutUp", "fadeIn", "fadeInDown", "fadeInDownBig", "fadeInLeft", "fadeInLeftBig", "fadeInRight", "fadeInRightBig", "fadeInUp", "", "fadeInUpBig", "fadeOut", "fadeOutDown", "fadeOutDownBig", "fadeOutLeft", "fadeOutLeftBig", "fadeOutRight", "fadeOutRightBig", "fadeOutUp", "fadeOutUpBig", "flipInX", "flipInY", "flipOutX", "flipOutY", "lightSpeedIn", "lightSpeedOut", "rotateIn", "rotateInDownLeft", "rotateInDownRight", "rotateInUpLeft", "rotateInUpRight", "rotateOut", "rotateOutDownLeft", "rotateOutDownRight", "rotateOutUpLeft", "rotateOutUpRight", "hinge", "jackInTheBox", "rollIn", "rollOut", "zoomIn", "zoomInDown", "zoomInLeft", "zoomInRight", "zoomInUp", "zoomOut", "zoomOutDown", "zoomOutLeft", "zoomOutRight", "zoomOutUp", "slideInDown", "slideInLeft", "slideInRight", "slideInUp", "slideOutDown", "slideOutLeft", "slideOutRight", "slideOutUp", "heartBeat"]
   },
   upper(e) {
     console.log(e)
@@ -53,43 +52,70 @@ Page({
   },
 
   scroll(e) {
-   // console.log(e)
+    //console.log(e)
+    let i = e.detail.scrollTop
+    let a = i / this.data.scrollHeight
+    // console.log(a)
+    if (this.isInteger(a)) {
+      this.changeClass()
+      this.setData({
+        scrollTop: this.data.scrollHeight * a
+      })
+    } else {
+      this.setData({
+        scrollTop: this.data.scrollHeight * parseInt(a)
+      })
+    }
+  },
+  //js判断是否是正整数
+  isInteger(number) {
+    return number > 0 && String(number).split('.')[1] == undefined
   },
 
   scrollToTop() {
-    console.log("1111")
     this.setAction({
       scrollTop: 0
     })
   },
 
   skip(e) {
-    console.log(e)
     let that = this;
     let index = e.currentTarget.dataset.index;
-    index ++
+    index++
     let id = "index" + index;
-    console.log(id)
+
+    that.changeClass()
+    // console.log(id)
     that.setData({
       toIndex: id
     })
-    if (index == that.data.imgList.length){
+    if (index == that.data.imgList.length) {
       that.setData({
-        toIndex: "index0"
+        toIndex: "index0",
       })
     }
+  },
+  changeClass() {
+    const max = this.data.classList.length
+    // parseInt(Math.random() * (max + 1), 10);
+    let a = Math.floor(Math.random() * (max + 1)); //取随机数
+    let _class = this.data.classList[a] + ' 2s infinite';
+    this.setData({
+      animation: _class
+    })
   },
   // changePage() {
   //   wx.redirectTo({
   //     url: "../test/test"
   //   })
   // },
- 
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
     var that = this;
+    that.changeClass()
     //获取系统的参数，scrollHeight数值,微信必须要设置style:height才能监听滚动事件
     wx.getSystemInfo({
       success: function(res) {
